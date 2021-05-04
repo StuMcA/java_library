c3c3e3d8ef09f9894f1fe0441fe496847cd57ea1
StuMcA <stuart.mcara@outlook.com>
2021-05-04 16:56:45 +0100

    initial commit

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookTest {
+
+    private Book book;
+    private Library library;
+
+    @Before
+    public void setUp() {
+        book = new Book("The Fellowship of the Ring", "J.R.R. Tolkein", "Fantasy");
+        library = new Library();
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("The Fellowship of the Ring", book.getName());
+    }
+
+    @Test
+    public void hasAuthor() {
+        assertEquals("J.R.R. Tolkein", book.getAuthor());
+    }
+    @Test
+    public void hasGenre() {
+        assertEquals("Fantasy", book.getGenre());
+    }
+
+}
